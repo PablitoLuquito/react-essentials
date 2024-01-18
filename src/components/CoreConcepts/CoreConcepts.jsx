b5d4e6e63eaef690161837cd6fca687752f89227
PablitoLuquito <pablolucas-1@outlook.com>
2024-01-18 08:01:29 -0300

    Commit inicial

---
+++ src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,16 @@
+import { CORE_CONCEPTS } from "../../data";
+import Section from "../Section/Section";
+import CoreConcept from "./CoreConcept";
+import "./CoreConcepts.css";
+
+export default function CoreConcepts() {
+  return (
+    <Section title="Core Concepts" id="core-concepts">
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+      </ul>
+    </Section>
+  );
+}
